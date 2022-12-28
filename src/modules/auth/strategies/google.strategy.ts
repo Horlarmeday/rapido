fa0cdc945b86ab6b4a86ee39581adac7405dbf1f
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2022-12-28 13:01:55 +0100

    feat: Add google strategy

---
+++ src/modules/auth/strategies/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback, Profile } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    console.log({ name, emails });
+    const user = {
+      email: emails?.[0]?.value,
+      first_name: name?.givenName,
+      last_name: name?.familyName,
+      accessToken,
+    };
+    return done(null, user);
+  }
+}
