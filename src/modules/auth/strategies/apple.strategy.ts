1fcad1518588f05a0d08d71a632c8bad27e2214e
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2022-12-28 12:58:09 +0100

    feat: Add apple strategy

---
+++ src/modules/auth/strategies/apple.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from '@arendajaelu/nestjs-passport-apple';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
+  constructor(config: ConfigService) {
+    super({
+      clientID: config.get<string>('APPLE_CLIENT_ID'),
+      teamID: config.get<string>('APPLE_TEAM_ID'),
+      keyID: config.get<string>('APPLE_KEY_ID'),
+      keyFilePath: config.get<string>('APPLE_KEYFILE_PATH'),
+      callbackURL: config.get<string>('APPLE_CALLBACK'),
+      passReqToCallback: false,
+      scope: ['email', 'name'],
+    });
+  }
+}
