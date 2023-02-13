e3026a8c8c33c5c413342c47e63b4b1e1d757081
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-02-13 07:50:39 +0100

    feat: Add admin settings module

---
+++ src/modules/admin-settings/admin-settings.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AdminSettingsService } from './admin-settings.service';
+import { AdminSettingsController } from './admin-settings.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  AdminSetting,
+  AdminSettingSchema,
+} from './entities/admin-setting.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: AdminSetting.name, schema: AdminSettingSchema },
+    ]),
+  ],
+  controllers: [AdminSettingsController],
+  providers: [AdminSettingsService],
+})
+export class AdminSettingsModule {}
