6ceb24f51d3d5f48e5cc6c323f9b2c7f8ce988e4
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-04-25 22:51:33 +0100

    feat: Add promotions module

--- src/modules/promotions/dto/change-promotion-status.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { Types } from 'mongoose';
-
-export class ChangePromotionStatusDto {
-  @IsNotEmpty()
-  @IsString()
-  promotionId: Types.ObjectId;
-}
--- src/modules/promotions/dto/delete-promotion.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { Types } from 'mongoose';
-
-export class DeletePromotionDto {
-  @IsNotEmpty()
-  @IsString()
-  promotionId: Types.ObjectId;
-}
